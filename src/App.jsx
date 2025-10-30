79a473d3e236ff267737da862f231bc60fe59189
Marcelo Aguero <marcelo_cr_n1@hotmail.com>
2025-10-30 16:50:16 -0300

    entra2e

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import './App.css'
 
 function App() {
@@ -9,8 +10,17 @@ function App() {
 
   return (
     <div className="app">
-      <NavBar />
-      <ItemListContainer greetings={"La mejor tienda virtual"} />
+      <BrowserRouter>
+        <NavBar />
+      
+        <Routes>
+          <Route path="/" element={ <ItemListContainer greeting={"Bienvenidos a la mejor tienda digital"} /> } />
+          <Route path="/category/:category" element={ <ItemListContainer greeting={"Bienvenidos a la mejor tienda digital"} /> } />
+          <Route path="/detail/:id" element={ <ItemDetailContainer /> } />
+          <Route path="*" element={ <div>Error 404</div>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
